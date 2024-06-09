3895e2a0a9721ed0e124ea319a24fe87c8c227bf
ShubhamNakashe <shubhamnakashe10@gmail.com>
2024-06-10 00:35:10 +0530

    host

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,22 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
+import { copyFileSync } from 'fs';
 
-// https://vitejs.dev/config/
-export default {
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html')
+      },
+    },
+  },
   base: './',
-};
+});
 
+// Copy _redirects file after build
+if (process.env.NODE_ENV === 'production') {
+  copyFileSync('public/_redirects', 'dist/_redirects');
+}
