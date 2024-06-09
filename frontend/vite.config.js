3c0a6426796313f8702016a91e3e00af6b08b2bd
ShubhamNakashe <shubhamnakashe10@gmail.com>
2024-06-10 00:45:11 +0530

    host

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,21 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import { VitePluginStaticCopy } from 'vite-plugin-static-copy';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    VitePluginStaticCopy({
-      targets: [
-        {
-          src: 'public/_redirects',
-          dest: ''
-        }
-      ]
-    })
-  ],
-  build: {
-    outDir: 'dist',
-  },
-  base: './',
+  plugins: [react()],
 });
