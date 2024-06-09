60bf39c2fc539e996243133b86e845d02fa72add
ShubhamNakashe <shubhamnakashe10@gmail.com>
2024-06-10 00:25:28 +0530

    hosting

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,16 +1,8 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-export default defineConfig({
-  root: '.', // Set the root to the current directory where index.html is located
-  plugins: [react()],
-  build: {
-    outDir: 'dist', // Output directory
-    rollupOptions: {
-      input: 'index.html', // Adjust the path if necessary
-    },
-  },
-  server: {
-    historyApiFallback: true, // For SPA routing
-  },
-});
+// https://vitejs.dev/config/
+export default {
+  base: './',
+};
+
