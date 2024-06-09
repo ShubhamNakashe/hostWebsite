6ff2f88f4ca2f9105439a4a8a504aa1d20d07e54
ShubhamNakashe <shubhamnakashe10@gmail.com>
2024-06-10 00:53:53 +0530

    stack overflow best

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,23 @@
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// });
+
 import { defineConfig } from "vite";
+import { resolve } from "path";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+      },
+    },
+  },
   plugins: [react()],
 });
