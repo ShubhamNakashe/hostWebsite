e4c6f5ff56d5fa13d90d6847d561b2a6a40c1479
ShubhamNakashe <shubhamnakashe10@gmail.com>
2024-06-09 13:38:45 +0530

    db

--- db.js
+++ db.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
-const mongoURI = "mongodb://127.0.0.1:27017";
+require('dotenv').config();
+const mongoURI = process.env.mongoURL;
 
 const connectToMongo = async () => {
     try {
